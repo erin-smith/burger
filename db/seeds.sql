3a61411aceeee01001f150f279405efbfa666243
Erin Smith <erin.acumen@gmail.com>
2020-06-26 17:34:48 -0700

    initial MVC setup

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Blue Cheese Burger', false);
+INSERT INTO burgers (burger_name) VALUES ('Double double Animal Style Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Dry Aged Bone Marrow Burger', false);
+INSERT INTO burgers (burger_name) VALUES ('Crispy Onion Mushroom Swiss Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon Burger', false);
+INSERT INTO burgers (burger_name) VALUES ('Fig Tomato Jam Burger');
