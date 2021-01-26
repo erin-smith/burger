b6517efb605e741ef75df8569b74cc75428a6e19
Erin Smith <erin.acumen@gmail.com>
2021-01-26 08:54:52 -0800

    fix syntax in schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ CREATE DATABASE wdkvljkwji8f7wh3;
 USE wdkvljkwji8f7wh3;
 
 CREATE TABLE burgers (
-	id INTEGER NOT NULL AUTO_INCREMENT,
+	id INTEGER(11) AUTO_INCREMENT NOT NULL,
 	burger_name VARCHAR(40) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+	PRIMARY KEY(id)
 );
