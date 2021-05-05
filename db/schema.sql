2fec47cc21071ddbffbe66966971456e8694a479
Erin Smith <erin.acumen@gmail.com>
2021-05-05 04:55:35 -0700

    edge case fixes - cannot add repeat burgers

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,9 @@ CREATE DATABASE wdkvljkwji8f7wh3;
 USE wdkvljkwji8f7wh3;
 
 CREATE TABLE burgers (
-	id INTEGER(11) AUTO_INCREMENT NOT NULL,
-	burger_name VARCHAR(40) NOT NULL,
+	id INTEGER(11) NOT NULL AUTO_INCREMENT,
+    CONSTRAINT CHK_burger CHECK (burger_name <> ""),
+	burger_name VARCHAR(40) NOT NULL UNIQUE,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY(id)
 );
